94c838b1ac22192ab03751006e47c6c4bf46bdcf
Viralpatel1710 <viralp11710@gmail.com>
2025-06-18 09:16:42 +0530

    Add files via upload

---
+++ #factorial for n numbers using for loop.py
@@ -0,0 +1,8 @@
+#factorial for n numbers using for loop
+n = int(input("Enter a number: "))
+factorial = 1
+
+for i in range(1, n + 1):
+    factorial *= i
+
+print(f"Factorial of {n} is: {factorial}")
