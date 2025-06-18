e143f16e7bed548d0c929e688271bc3c9e2f575b
Viralpatel1710 <viralp11710@gmail.com>
2025-06-18 09:17:28 +0530

    Add files via upload

---
+++ #print element[1,4,9,16,25,36,49,64,81,1.py
@@ -0,0 +1,8 @@
+#print element[1,4,9,16,25,36,49,64,81,100]
+
+nums = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+idx = 0
+
+while idx < len(nums):  # Use 'len', not 'lenth', and no semicolon
+    print(nums[idx])
+    idx += 1
