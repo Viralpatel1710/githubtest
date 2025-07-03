6f27dd23baed1bf042f0378d8c4d8134e577611a
Viralpatel1710 <viralp11710@gmail.com>
2025-07-03 16:36:08 +0530

    Add files via upload

---
+++ number guessing.py
@@ -0,0 +1,26 @@
+import random
+
+def guess_the_number():
+    number_to_guess = random.randint(1, 100)
+    guess = None
+    attempts = 0
+
+    print(" Welcome to Guess the Number!")
+    print("I have selected a number between 1 and 100.")
+    
+    while guess != number_to_guess:
+        try:
+            guess = int(input("Enter your guess: "))
+            attempts += 1
+            
+            if guess < number_to_guess:
+                print(" Too low! Try again.")
+            elif guess > number_to_guess:
+                print(" Too high! Try again.")
+            else:
+                print(f" Correct! You guessed it in {attempts} tries.")
+        except ValueError:
+            print(" Please enter a valid number.")
+
+# Run the game
+guess_the_number()
